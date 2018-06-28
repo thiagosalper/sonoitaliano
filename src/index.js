40dae9ee1307557c6ec7204c9bd48be3f7de3144
Thiago Pereira <thiago_salper@hotmail.com>
2018-06-28 17:36:34 -0300

    onesignal added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import ConsulDetails from './containers/Consul/consuldetails';
 import Links from './containers/Consul/links';
 import ListaEspera from './containers/Consul/listaespera';
 
+import OneSignal from 'react-native-onesignal';
+
 const Route = createStackNavigator({
     Step: {screen: Step, 
         navigationOptions: {
@@ -55,6 +57,35 @@ const Route = createStackNavigator({
 });
 
 export default class App extends React.Component {
+    componentWillMount = () => {
+        OneSignal.init("553bdcdc-1dd5-4de0-8873-e3a3df73f23e");
+
+        OneSignal.addEventListener('received', this.onReceived);
+        OneSignal.addEventListener('opened', this.onOpened);
+        OneSignal.addEventListener('ids', this.onIds);
+    }
+
+    componentWillUnmount = () => {
+        OneSignal.removeEventListener('received', this.onReceived);
+        OneSignal.removeEventListener('opened', this.onOpened);
+        OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived = (notification) => {
+        console.log("Notification received: ", notification);
+    }
+
+    onOpened = (openResult) => {
+        console.log('Message: ', openResult.notification.payload.body);
+        console.log('Data: ', openResult.notification.payload.additionalData);
+        console.log('isActive: ', openResult.notification.isAppInFocus);
+        console.log('openResult: ', openResult);
+    }
+
+    onIds = (device) => {
+        console.log('Device info: ', device);
+    }
+
     render = () => {
         return <Route />;
     }
